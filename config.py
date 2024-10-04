177963b0a5a30e1d29429f1f59cd96faab01950a
Nicol <150965028+nicksasan@users.noreply.github.com>
2024-10-04 12:07:17 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = 'nisnita'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'Proyecto'
+    SESSION_COOKIE_SECURE = False
